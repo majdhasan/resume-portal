673bdf585060a319e066b541aad2973d7fb3afe5
majdhasan <majd.hasan@hotmail.com>
2021-07-19 22:13:28 +0200

    add user model and repository

--- src/main/java/life/majd/resumeportal/ResumePortalApplication.java
+++ src/main/java/life/majd/resumeportal/ResumePortalApplication.java
@@ -1,9 +1,12 @@
 package life.majd.resumeportal;
 
+import life.majd.resumeportal.repositories.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
 public class ResumePortalApplication {
 
 	public static void main(String[] args) {
