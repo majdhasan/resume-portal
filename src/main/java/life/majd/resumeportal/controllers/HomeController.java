05b5a85b71624816c41b71a0ac8c588b79837543
majdhasan <majd.hasan@hotmail.com>
2021-07-23 13:04:00 +0200

    add endpoints to render templates

--- src/main/java/life/majd/resumeportal/controllers/HomeController.java
+++ src/main/java/life/majd/resumeportal/controllers/HomeController.java
@@ -1,14 +1,27 @@
 package life.majd.resumeportal.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/edit")
+    public String edit() {
+        return "edit page";
+    }
+
+    @GetMapping("/view/{userId}")
+    public String view(@PathVariable String userId, Model model) {
+        model.addAttribute("userId", userId);
+        return "profile";
     }
 
 }
